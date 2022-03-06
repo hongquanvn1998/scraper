98b039f40c1d5e87a7cc1d30c8a42b148e8b2c85
Hoang Quan <hongquanvn1998@gmail.com>
2022-03-06 22:26:23 +0700

    update send message

--- send_message.py
+++ send_message.py
@@ -13,31 +13,34 @@ api_hash = config.api_hash
 list_phone = config.list_phone
 channel = config.channel
 send_to_channel = config.send_to_channel
-num = 1
+num = 0
 
-
-for data in list_phone:
-    phone = data['phone']
-    ip = data['ip']
-    port = data['port']
-    message = ''
-    client = TelegramClient("session/" + phone, api_id, api_hash)
-                            # proxy=(python_socks.ProxyType.SOCKS5, ip, port))
-    client.connect()
-    if not client.is_user_authorized():
-        print(F"Session lỗi!" + phone)
-        client.disconnect()
-        continue
-    else:
-        for i in channel:
-            client(JoinChannelRequest(i))
-        entity = client.get_entity(send_to_channel)
-        with open(r"question.csv", encoding='UTF-8') as f:
-            rows = csv.reader(f, delimiter=",", lineterminator="\n")
-            rows = list(rows)
-            message = rows[num][0]
-        client.send_message(entity=entity, message=str(message))
-        print(F"Sent to %s with message %s !" % (send_to_channel, message))
-        client.disconnect()
-        num += 1
-        sleep(600)
+with open(r"question.csv", encoding='UTF-8') as f:
+    rows = csv.reader(f, delimiter=",", lineterminator="\n")
+    # rows = list(rows)
+    # message = rows[num][0]
+    for row in rows:
+        message = row[0]
+        if num == len(list_phone):
+            num = 0
+        data = list_phone[num]
+        phone = data['phone']
+        ip = data['ip']
+        port = data['port']
+        message = ''
+        client = TelegramClient("session/" + phone, api_id, api_hash)
+                                # proxy=(python_socks.ProxyType.SOCKS5, ip, port))
+        client.connect()
+        if not client.is_user_authorized():
+            print(F"Session lỗi!" + phone)
+            client.disconnect()
+            continue
+        else:
+            for i in channel:
+                client(JoinChannelRequest(i))
+            entity = client.get_entity(send_to_channel)
+            client.send_message(entity=entity, message=str(message))
+            print(F"Sent to %s with message %s !" % (send_to_channel, message))
+            client.disconnect()
+            num += 1
+            sleep(300)
--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import csv
 
 with open(r"question.csv", encoding='UTF-8') as f:
     rows = csv.reader(f, delimiter=",", lineterminator="\n")
-    rows = list(rows)
-    message = rows[3][0]
-    print(message)
+    for row in rows:
+        print(row[0])
+        break
+    # rows = list(rows)
+    # message = rows[3][0]
+    # print(len(rows))
