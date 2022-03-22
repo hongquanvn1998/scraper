a4c12fc73a4cd06e281da43bd18ad72ead732bba
Hoang Quan <hongquanvn1998@gmail.com>
2022-03-22 22:51:33 +0700

    fix send async

--- send_message.py
+++ send_message.py
@@ -15,8 +15,6 @@ send_to_channel = sys.argv[1]
 file_message = sys.argv[2]
 file_session = sys.argv[3]
 file_phone = sys.argv[4]
-num = 0
-number_proxy = 0
 
 proxies = []
 with open('proxies.txt') as f:
@@ -37,13 +35,15 @@ with open('%s' % file_phone) as f:
         list_phone.append(line)
 
 
-async def __main__():
+async def __main__(_list_phone, _proxies):
+    num = 0
+    number_proxy = 0
     with open(r"%s" % file_message, encoding='UTF-8') as f:
         rows = csv.reader(f, delimiter=",", lineterminator="\n")
         for row in rows:
             message = row[0]
-            phone = list_phone[num]
-            get_proxy = proxies[number_proxy]
+            phone = _list_phone[num]
+            get_proxy = _proxies[number_proxy]
             try:
                 if file_session == 'fanpad':
                     client = TelegramClient("%s/%s" % (file_session, phone), api_id, api_hash,
@@ -54,10 +54,11 @@ async def __main__():
                                             proxy=(python_socks.ProxyType.SOCKS5, get_proxy['ip'], get_proxy['port'], True,
                                                    get_proxy['user'], get_proxy['password']))
                 await client.connect()
+                await client.start()
                 if not await client.is_user_authorized():
                     print(F"Session lỗi!" + phone)
-                    num = 0 if num == len(list_phone) - 1 else num + 1
-                    number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
+                    num = 0 if num == len(_list_phone) - 1 else num + 1
+                    number_proxy = number_proxy + 1 if number_proxy < len(_proxies) - 1 else 0
                     await client.disconnect()
                     continue
                 else:
@@ -65,18 +66,18 @@ async def __main__():
                     entity = await client.get_entity(send_to_channel)
                     await client.send_message(entity=entity, message=str(message))
                     print(F"%s: Sent to %s with message %s !" % (phone, send_to_channel, message))
-                    num = 0 if num == len(list_phone) - 1 else num + 1
-                    number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
+                    num = 0 if num == len(_list_phone) - 1 else num + 1
+                    number_proxy = number_proxy + 1 if number_proxy < len(_proxies) - 1 else 0
                     await client.disconnect()
                     if send_to_channel == 'coinpassion':
                         sleep(1200)
                     else:
                         sleep(600)
             except Exception as e:
-                print('%s Something Error: %s'% (list_phone[num], e))
-                num = 0 if num == len(list_phone) - 1 else num + 1
-                number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
+                print('%s Something Error: %s' % (_list_phone[num], e))
+                num = 0 if num == len(_list_phone) - 1 else num + 1
+                number_proxy = number_proxy + 1 if number_proxy < len(_proxies) - 1 else 0
 
 loop = asyncio.new_event_loop()
-loop.run_until_complete(__main__())
+loop.run_until_complete(__main__(list_phone,proxies))
 loop.close()
