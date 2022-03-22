59c1b083e62acb7537f65766fbf0be27dbdf45d2
Hoang Quan <hongquanvn1998@gmail.com>
2022-03-22 22:46:03 +0700

    fix send async

--- send_message.py
+++ send_message.py
@@ -36,39 +36,47 @@ with open('%s' % file_phone) as f:
     for line in lines:
         list_phone.append(line)
 
-with open(r"%s" % file_message, encoding='UTF-8') as f:
-    rows = csv.reader(f, delimiter=",", lineterminator="\n")
-    for row in rows:
-        message = row[0]
-        phone = list_phone[num]
-        get_proxy = proxies[number_proxy]
-        try:
-            if file_session == 'fanpad':
-                client = TelegramClient("%s/%s" % (file_session, phone), api_id, api_hash,
-                                        proxy=(python_socks.ProxyType.SOCKS5, get_proxy['ip'], get_proxy['port'], True,
-                                               get_proxy['user'], get_proxy['password']))
-            else:
-                client = TelegramClient("%s/%s/%s" % (file_session, phone, phone), api_id, api_hash,
-                                        proxy=(python_socks.ProxyType.SOCKS5, get_proxy['ip'], get_proxy['port'], True,
-                                               get_proxy['user'], get_proxy['password']))
-            client.connect()
-            if not client.is_user_authorized():
-                print(F"Session lỗi!" + phone)
-                num = 0 if num == len(list_phone) - 1 else num + 1
-                number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
-                client.disconnect()
-                continue
-            else:
-                client(JoinChannelRequest(send_to_channel))
-                entity = client.get_entity(send_to_channel)
-                client.send_message(entity=entity, message=str(message))
-                print(F"%s: Sent to %s with message %s !" % (phone, send_to_channel, message))
-                num = 0 if num == len(list_phone) - 1 else num + 1
-                number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
-                client.disconnect()
-                if send_to_channel == 'coinpassion':
-                    sleep(1200)
+
+async def __main__():
+    with open(r"%s" % file_message, encoding='UTF-8') as f:
+        rows = csv.reader(f, delimiter=",", lineterminator="\n")
+        for row in rows:
+            message = row[0]
+            phone = list_phone[num]
+            get_proxy = proxies[number_proxy]
+            try:
+                if file_session == 'fanpad':
+                    client = TelegramClient("%s/%s" % (file_session, phone), api_id, api_hash,
+                                            proxy=(python_socks.ProxyType.SOCKS5, get_proxy['ip'], get_proxy['port'], True,
+                                                   get_proxy['user'], get_proxy['password']))
                 else:
-                    sleep(600)
-        except Exception as e:
-            print('Something Error: ',e)
+                    client = TelegramClient("%s/%s/%s" % (file_session, phone, phone), api_id, api_hash,
+                                            proxy=(python_socks.ProxyType.SOCKS5, get_proxy['ip'], get_proxy['port'], True,
+                                                   get_proxy['user'], get_proxy['password']))
+                await client.connect()
+                if not await client.is_user_authorized():
+                    print(F"Session lỗi!" + phone)
+                    num = 0 if num == len(list_phone) - 1 else num + 1
+                    number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
+                    await client.disconnect()
+                    continue
+                else:
+                    await client(JoinChannelRequest(send_to_channel))
+                    entity = await client.get_entity(send_to_channel)
+                    await client.send_message(entity=entity, message=str(message))
+                    print(F"%s: Sent to %s with message %s !" % (phone, send_to_channel, message))
+                    num = 0 if num == len(list_phone) - 1 else num + 1
+                    number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
+                    await client.disconnect()
+                    if send_to_channel == 'coinpassion':
+                        sleep(1200)
+                    else:
+                        sleep(600)
+            except Exception as e:
+                print('%s Something Error: %s'% (list_phone[num], e))
+                num = 0 if num == len(list_phone) - 1 else num + 1
+                number_proxy = number_proxy + 1 if number_proxy < len(proxies) - 1 else 0
+
+loop = asyncio.new_event_loop()
+loop.run_until_complete(__main__())
+loop.close()
